64a8e373f00a468238639a952c8728c04b5e3df6
Steven Nguyen <qviet715@gmail.com>
2023-04-09 14:46:16 -0400

    index seeds

---
+++ seeds/index.js
@@ -0,0 +1,23 @@
+const seedHobbyBox = require('./hobbybox-seeds');
+const seedSubscriptions = require('./subscriptions-seeds');
+const seedUsers = require('./users-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+
+  await seedHobbyBox();
+  console.log('\n----- HOBBYBOX SEEDED -----\n');
+
+  await seedUsers();
+  console.log('\n----- USERS SEEDED -----\n');
+
+  await seedSubscriptions();
+  console.log('\n----- SUBSCRIPTIONS SEEDED -----\n');
+
+  process.exit(0);
+};
+
+seedAll();
