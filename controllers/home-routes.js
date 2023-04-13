8e7061718b473816d0e050bcd3b4a8e30c28f734
Steven Nguyen <qviet715@gmail.com>
2023-04-13 07:54:19 -0400

    add require session loggedin for login, signup, hobbies, and made a logout route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,38 +1,62 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection.js');
+const router = require("express").Router();
+const sequelize = require("../config/connection.js");
 const { Users, HobbyBox, Subscriptions } = require("../models");
 
 //this will be where we add all front end routes
 
 //route for homepage   NO AUTH YET
-router.get('/', (req, res) => {
-  res.render('homePage');
+router.get("/", (req, res) => {
+  if (req.session.loggedIn) {
+    res.render("hobbies", {
+      loggedIn: req.session.loggedIn,
+    });
+    return;
+  }
+  // res.render('homePage'); ->want user to be directed to hobbies after signed up or logged in
 });
 
-//route for signup page      this is where we whill need req.session.loggedIn
+//route for signup page this is where we whill need req.session.loggedIn
 
-router.get('/signup', (req, res) => {
-  res.render('signUp')
-})
-
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/hobbies");
+    return;
+  }
+  res.render("signUp");
+});
 
 //route for login
-router.get('/login', (req, res) => {
-  res.render('login')
-})
-
-//route for sign up
-router.get('/signup', (req, res) => {
-  res.render('signUp')
-})
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/hobbies");
+    return;
+  }
+  res.render("login");
+});
 
 //route for hobbies page
 // all hobbies we have available here with pics 4-10 ?? idk
-router.get('/hobbies', (req, res) => {
-  res.render('hobbies')
-})
+router.get("/hobbies", (req, res) => {
+  if (req.session.loggedIn) {
+    res.render("hobbies", {
+      loggedIn: req.session.loggedIn,
+    });
+    return;
+  }
+  // res.render('hobbies')
+  res.redirect("/");
+});
 
+//logout route
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
-//routes for etc......
 
 module.exports = router;
