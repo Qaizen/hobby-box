2602b03e2d2a4f1198359054e054b7c59cf39b7d
Emilio Regalado <quimbolo92@yahoo.com>
2023-04-15 16:44:58 -0400

    updated handlebars, and images

--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-function chageBackgroundImageRandomly() {
-    console.log('loaded page');
-    let myDiv = document.querySelector('body');
-    var image = 'https://source.unsplash.com/1600x900/?landscape'
-    myDiv.style.backgroundImage = "url('" + image + "')";
-}
-window.addEventListener('load', chageBackgroundImageRandomly)
